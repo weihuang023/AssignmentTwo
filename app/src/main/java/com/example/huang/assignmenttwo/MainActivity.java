59ea57eb946acd931c37f42ee2067ada37b05b14
weihuang023 <weih81888@gmail.com>
2016-02-28 22:01:53 -0500

    Update Assignment Two

--- app/src/main/java/com/example/huang/assignmenttwo/MainActivity.java
+++ app/src/main/java/com/example/huang/assignmenttwo/MainActivity.java
@@ -8,12 +8,14 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements OnClickListener {
-
+/**
+ * If you want to change Picture, go to res/drawable then copy and paste the file
+ * If you want to change text in about class. go to res/values/strings then edit text
+ * */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         Button aboutButton = (Button) findViewById(R.id.aboutButton);
         aboutButton.setOnClickListener(this);
         Button exitButton = (Button) findViewById(R.id.exitButton);
@@ -24,16 +26,13 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
     @Override
     public void onClick(View v) {
         switch (v.getId()){
-
             case R.id.exitButton:
                 finish();
                 break;
-
             case R.id.aboutButton:
                 Intent i = new Intent(this, About.class);
                 startActivity(i);
                 break;
-
         }
     }
 }
